90c85385255f74acf143425feda8c7325f0306fa
Anna Tolochko <anya.pogorelova@gmail.com>
2023-03-19 21:46:01 +0200

    formatting and minor fixes

--- app.js
+++ app.js
@@ -1,73 +1,73 @@
 actions = [
-    'прибрати <acc>',
-    'демонтувати <acc>',
-    'посприяти демонтажу <gen>',
-    'здійснити демонтаж <gen>'
+    "прибрати <acc>",
+    "демонтувати <acc>",
+    "посприяти демонтажу <gen>",
+    "здійснити демонтаж <gen>"
 ]
 
 from_object = [
-    'з будинку',
-    'з фасаду будинку',
-    'з будівлі'
+    "з будинку",
+    "з фасаду будинку",
+    "з будівлі"
 ]
 
 location_options = [
-    ' за адресою',
-    ', що знаходиться за адресою',
-    ', що знаходиться на'
+    " за адресою",
+    ", що знаходиться за адресою",
+    ", що знаходиться на"
 ]
 
 conditioner_options = {
-    'acc_single': 'кондиціонер',
-    'acc_mult': 'кондиціонери',
-    'gen_single': 'кондиціонера',
-    'gen_mult': 'кондиціонерів'
+    "acc_single": "кондиціонер",
+    "acc_mult": "кондиціонери",
+    "gen_single": "кондиціонера",
+    "gen_mult": "кондиціонерів"
 }
 
 antenna_options = {
-    'acc_single': 'антенy',
-    'acc_mult': 'антени',
-    'gen_single': 'антени',
-    'gen_mult': 'антен'
+    "acc_single": "антенy",
+    "acc_mult": "антени",
+    "gen_single": "антени",
+    "gen_mult": "антен"
 }
 
 roof_options = {
-    'acc_single': 'дашок',
-    'acc_mult': 'дашки',
-    'gen_single': 'дашка',
-    'gen_mult': 'дашків'
+    "acc_single": "дашок",
+    "acc_mult": "дашки",
+    "gen_single": "дашка",
+    "gen_mult": "дашків"
 }
 
 tech_elements_options = {
-    'acc_mult': 'технічні елементи',
-    'gen_mult': 'технічних елементів'
+    "acc_mult": "технічні елементи",
+    "gen_mult": "технічних елементів"
 }
 
-conj = ['і', 'та']
+conj = ["і", "та"]
 
 doc_name_options = [
-    '№952 від 19.10.2022',
-    '№952 від 19/10/2022',
-    '№952 від 10/19/2022'
+    "№952 від 19.10.2022",
+    "№952 від 19/10/2022",
+    "№952 від 10/19/2022"
 ];
 
 ref_options = [
-    'згідно рішення ЛМР',
-    'у відповідності до рішення ЛМР',
-    'згідно із рішенням ЛМР',
-    'згідно з рішенням ЛМР'
+    "згідно рішення ЛМР",
+    "у відповідності до рішення ЛМР",
+    "згідно із рішенням ЛМР",
+    "згідно з рішенням ЛМР"
 ];
 
 tech_elements_addon_options = [
-    '(разом з кріпленнями, дротами та іншими залишками)',
-    '(разом із дротами та кріпленнями)',
-    '(разом із дротами та кріпленнями, а також іншими залишками)',
-    '(з кріпленнями і дротами)',
-    '(з дротами та кріпленнями)'
+    "(разом з кріпленнями, дротами та іншими залишками)",
+    "(разом із дротами та кріпленнями)",
+    "(разом із дротами та кріпленнями, а також іншими залишками)",
+    "(з кріпленнями і дротами)",
+    "(з дротами та кріпленнями)"
 ]
 
-greetings = ['Доброго дня!', 'Вітаю!', 'Добрий день!'];
-thanks = ['Дякую!', 'Дякую.']
+greetings = ["Доброго дня!", "Вітаю!", "Добрий день!"];
+thanks = ["Дякую!", "Дякую."]
 
 
 function shuffleArray(array) {
@@ -86,48 +86,48 @@ function selectRandom(stringsArray) {
 
 
 function getElement(options, grammatic_case, is_multiple) {
-    obj_type = is_multiple ? 'mult' : 'single';
+    obj_type = is_multiple ? "mult" : "single";
     return options[`${grammatic_case}_${obj_type}`];
 }
 
 
 function getTechElementText(params, grammatic_case) {
-    if (params['hasTechElements']) {
+    if (params["hasTechElements"]) {
         elementsText = tech_elements_options[`${grammatic_case}_mult`];
         addonText = selectRandom(tech_elements_addon_options);
-        elementsText += ' ' + addonText;
+        elementsText += " " + addonText;
 
         return elementsText;
     } else {
         elements = [];
 
-        if (params['hasConditioner']) {
-            elements.push(getElement(conditioner_options, grammatic_case, params['hasMultipleConditioners']));
+        if (params["hasConditioner"]) {
+            elements.push(getElement(conditioner_options, grammatic_case, params["hasMultipleConditioners"]));
         }
 
-        if (params['hasAntenna']) {
-            elements.push(getElement(antenna_options, grammatic_case, params['hasMultipleAntennas']));
+        if (params["hasAntenna"]) {
+            elements.push(getElement(antenna_options, grammatic_case, params["hasMultipleAntennas"]));
         }
 
-        if (params['hasRoof']) {
-            elements.push(getElement(roof_options, grammatic_case, params['hasMultipleRoofs']));
+        if (params["hasRoof"]) {
+            elements.push(getElement(roof_options, grammatic_case, params["hasMultipleRoofs"]));
         }
 
         rand_conj = selectRandom(conj);
         elements = shuffleArray(elements);
 
-        elementsText = '';
+        elementsText = "";
 
         if (elements.length > 2) {
-            elementsText = elements.slice(0, elements.length - 1).join(', ');
+            elementsText = elements.slice(0, elements.length - 1).join(", ");
             elementsText = `${elementsText} ${rand_conj} ${elements[elements.length - 1]}`;
         } else {
             elementsText = elements.join(` ${rand_conj} `);
         }
 
-        if (params['hasConditioner'] || params['hasAntenna']) {
+        if (params["hasConditioner"] || params["hasAntenna"]) {
             addonText = selectRandom(tech_elements_addon_options);
-            elementsText += ' ' + addonText;
+            elementsText += " " + addonText;
         }
 
         return elementsText;
@@ -148,29 +148,29 @@ function capitalizeFirstLetter(string) {
 
 function getRequestBody(params) {
     // Request
-    text = 'прошу';
+    text = "прошу";
 
     // Action
     action = selectRandom(actions);
     gramatic_case_regex = /<(.*)>/g;
-    grammatic_case = action.match(gramatic_case_regex)[0].replace('<', '').replace('>', '');
+    grammatic_case = action.match(gramatic_case_regex)[0].replace("<", "").replace(">", "");
 
     // Tech elements
     elementsText = getTechElementText(params, grammatic_case);
-    text += ' ';
+    text += " ";
     text += action.replace(gramatic_case_regex, elementsText);
     text += ` ${selectRandom(from_object)}`;
 
     // UNESCO
-    if (params['isUnesco']) {
-        text += ' - частини об\'єкта Всесвітньої спадщини ЮНЕСКО (пам\'ятки національного значення)';
-    } else if (params['isArchitecturalMonument']) {
-        text += ' - пам\'ятки';
+    if (params["isUnesco"]) {
+        text += " - частини об'єкта Всесвітньої спадщини ЮНЕСКО (пам'ятки національного значення)";
+    } else if (params["isArchitecturalMonument"]) {
+        text += " - пам'ятки";
     }
 
     // Address
     rand_location = selectRandom(location_options);
-    text += `${rand_location} ${params['address']}`;
+    text += `${rand_location} ${params["address"]}`;
 
     return text;
 }
@@ -178,7 +178,7 @@ function getRequestBody(params) {
 
 function generateAppealText(params) {
     // Greeting
-    greeting = '';
+    greeting = "";
     add_greeting = selectRandom([true, false]);
 
     if (add_greeting) {
@@ -189,13 +189,13 @@ function generateAppealText(params) {
     referenceBody = getReferenceText();
 
     body = shuffleArray([requestBody, referenceBody]);
-    appealText = body.join(', ');
+    appealText = body.join(", ");
     appealText = capitalizeFirstLetter(appealText);
 
-    if (greeting != '') {
+    if (greeting != "") {
         appealText = `${greeting} ${appealText}.`;
     } else {
-        appealText += '.';
+        appealText += ".";
     }
 
     return appealText;
